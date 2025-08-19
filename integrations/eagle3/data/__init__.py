44990ff1ed24c94a61e75d3fff022563b3865c90
wengwanjiang <wengwanjiang@gmail.com>
2025-08-19 12:44:23 +0000

    fix bug

--- integrations/eagle3/data/__init__.py
+++ integrations/eagle3/data/__init__.py
@@ -1,13 +1,10 @@
 from .preprocessing import (
     build_eagle3_dataset,
-    # build_offline_eagle3_dataset,
     generate_vocab_mapping_file,
 )
-from .utils import prepare_dp_dataloaders, Eagle3DataCollatorWithPadding
+from .utils import Eagle3DataCollatorWithPadding
 
 __all__ = [
     "build_eagle3_dataset",
-    # "build_offline_eagle3_dataset",
-    "generate_vocab_mapping_file",
-    "prepare_dp_dataloaders",
+    "generate_vocab_mapping_file"
 ]
